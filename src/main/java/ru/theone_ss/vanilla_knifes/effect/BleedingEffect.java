6a11ac3f1213ab95afc4b4d68117378f522205b3
Theoness1 <sasha.bolotskih2002@gmail.com>
2022-06-21 17:12:23 +0700

    add particles

--- src/main/java/ru/theone_ss/vanilla_knifes/effect/BleedingEffect.java
+++ src/main/java/ru/theone_ss/vanilla_knifes/effect/BleedingEffect.java
@@ -3,6 +3,8 @@ package ru.theone_ss.vanilla_knifes.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import ru.theone_ss.vanilla_knifes.registry.VanillaKnifesEffects;
 
 public class BleedingEffect extends BaseStatusEffect{
@@ -14,6 +16,11 @@ public class BleedingEffect extends BaseStatusEffect{
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
         if(this == VanillaKnifesEffects.BLEEDING) {
             entity.damage(DamageSource.MAGIC, 1.0F);
+        }
+        if(entity.getWorld() instanceof ServerWorld serverWorld) {
+            serverWorld.spawnParticles(ParticleTypes.DAMAGE_INDICATOR, entity.getX() - 0.3, entity.getY() + 0.5, entity.getZ() - 0.4, 1, 0.0, 0.0, 0, 0);
+            serverWorld.spawnParticles(ParticleTypes.DAMAGE_INDICATOR, entity.getX() + 0.6, entity.getY() + 0.4, entity.getZ() + 0.1, 1, 0.0, 0.0, 0, 0);
+            serverWorld.spawnParticles(ParticleTypes.DAMAGE_INDICATOR, entity.getX() - 0.6, entity.getY() + 0.3, entity.getZ() + 0.3, 1, 0.0, 0.0, 0, 0);
         }
     }
 
