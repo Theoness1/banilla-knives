a7393f6411e860e009dff19490040f78f47c5646
Theoness1 <sasha.bolotskih2002@gmail.com>
2022-06-21 17:44:45 +0700

    fix crush

--- src/main/java/ru/theone_ss/vanilla_knifes/compat/WinterlyIntegration.java
+++ src/main/java/ru/theone_ss/vanilla_knifes/compat/WinterlyIntegration.java
@@ -2,12 +2,13 @@ package ru.theone_ss.vanilla_knifes.compat;
 
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import ru.theone_ss.vanilla_knifes.item.material.BaseToolMaterial;
-import ru.tlmclub.winterly.registry.WinterlyItems;
 
 public class WinterlyIntegration {
 
-    public static ToolMaterial CRYOMARBLE_MATERIAL = new BaseToolMaterial(1, 521, 2.0F, 2.0F, 6, () -> Ingredient.ofItems(WinterlyItems.CRYOMARBLE));
+    public static ToolMaterial CRYOMARBLE_MATERIAL = new BaseToolMaterial(1, 521, 2.0F, 2.0F, 6, () -> Ingredient.ofItems(Registry.ITEM.get(new Identifier("winterly", "cryomarble"))));
 
     public static ToolMaterial getMaterial() {
         return CRYOMARBLE_MATERIAL;
